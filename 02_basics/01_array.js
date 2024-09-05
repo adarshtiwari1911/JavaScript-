ec6397e03d9146332f064cd94246855eadc9600e
Adarsh Tiwari <145975230+adarshtiwari1911@users.noreply.github.com>
2024-09-05 17:04:15 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,16 @@
+//Arrays
+
+const myArr=[0,1,2,3,4,5,6]
+console.log(myArr)
+console.log(myArr[0])
+
+const myHeroes=["shaktiman","Naagraj"]
+
+const myArr2=new Array(1,2,3,4,5,6)
+console.log(myArr2)
+
+//array methods
+
+myArr.push(7)
+myArr.push()
+console.log(myArr)
