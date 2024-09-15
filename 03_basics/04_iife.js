58ce5ae4eda19e09f7294f58596949aaf977e536
Adarsh Tiwari <145975230+adarshtiwari1911@users.noreply.github.com>
2024-09-15 06:49:56 +0000

    iife done

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,15 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+// IIFE ka matlab hai ek function jo turant define hone ke baad execute ho jata hai
+
+(function chai() {
+    // yeh ek named IIFE hai
+    console.log(`DB CONNECTED`);
+    // yeh function turant execute hoga bina kisi call ke
+})();
+
+((name) => {
+    // yeh ek anonymous IIFE hai jo arrow function ka use kar raha hai
+    console.log(`DB CONNECTED TWO ${name}`);
+    // isme 'name' parameter ko pass kar rahe hain, jo execution ke samay me 'hitesh' hoga
+})('adarsh');
